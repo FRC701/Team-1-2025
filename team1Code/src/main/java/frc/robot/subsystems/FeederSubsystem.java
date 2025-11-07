2e6a589dd1a5752f278e8fd5accffead4426d3d1
calebdeguia <caleb@vandenrobotics.com>
2025-11-06 16:44:39 -0800

    Feeder Subsystem WIP
    Created: Methods, Motor
    Need: States, Define States, Switch Case, Periodic

---
+++ team1Code/src/main/java/frc/robot/subsystems/FeederSubsystem.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class FeederSubsystem extends SubsystemBase {
+  /** Creates a new FeederSubsystem. */
+
+  //Motor
+  public TalonFX FeederMotor;
+
+  public FeederSubsystem() {
+    FeederMotor = new TalonFX(0);
+  }
+
+  //Methods
+  public void MoveFeederMotor() {
+    FeederMotor.setVoltage(4);
+  }
+
+  public void StopFeederMotor() {
+    FeederMotor.setVoltage(0);
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}

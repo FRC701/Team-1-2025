28dde772369a72f4f0a74fe0cff2fcabdae557b0
k5noa <noa@vandenrobotics.com>
2025-11-20 16:13:26 -0800

    finished intake subsystem
    finished switch cases + added code to interact with banner sensor | removed commands because only the shooter part needs commands.

--- team1Code/src/main/java/frc/robot/subsystems/FeederSubsystem.java
+++ team1Code/src/main/java/frc/robot/subsystems/FeederSubsystem.java
@@ -4,18 +4,41 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.ForwardLimitValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class FeederSubsystem extends SubsystemBase {
   /** Creates a new FeederSubsystem. */
 
   //Motor
   public TalonFX FeederMotor;
+  private TalonFXConfiguration mTalonFXConfig;
+  // creates states for intake
+  public enum FeederStates {
+    S_Filled,S_NotFilled,S_Feeding
+  }
+  //assignes class or name or whatever to the states
+  public static FeederStates mFeederStates;
 
   public FeederSubsystem() {
-    FeederMotor = new TalonFX(0);
+    FeederMotor = new TalonFX(1);
+    //Lines below are tied to banner sensor
+    mTalonFXConfig = new TalonFXConfiguration();
+    mTalonFXConfig.HardwareLimitSwitch.ForwardLimitEnable = false;
+    FeederMotor.getConfigurator().apply(mTalonFXConfig);
+    mFeederStates = FeederStates.S_Filled;
+
+    var fx_cfg = new MotorOutputConfigs();
+
+    fx_cfg.NeutralMode = NeutralModeValue.Brake;
+
+    FeederMotor.getConfigurator().apply(fx_cfg);
   }
 
   //Methods
@@ -27,9 +50,41 @@ public class FeederSubsystem extends SubsystemBase {
     FeederMotor.setVoltage(0);
   }
 
+  public boolean revLimitStatus() {
+    return (FeederMotor.getForwardLimit().getValue() == ForwardLimitValue.ClosedToGround);
+  }
+
+  //switchcase
+
+  public void RunFeederState(){
+    switch (mFeederStates) {
+      case S_Filled:
+        StopFeederMotor();
+        break;
+        //if state is filled stop the motor
+      case S_NotFilled:
+        if(!revLimitStatus()){
+          mFeederStates = FeederStates.S_Filled;
+        }
+        else{
+        MoveFeederMotor();
+        }
+        break;
+      case S_Feeding:
+        if(revLimitStatus()){
+          mFeederStates = FeederStates.S_NotFilled;
+        }
+        else{
+        MoveFeederMotor();
+        }
+        break;
+        //if state is feeding set state to not filled & move motor
+    }
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    RunFeederState();
   }
 }
